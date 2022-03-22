29c909ed7094f428cb834fba9227b90728c6c9c3
Pakkito <pe@pierlis.com>
2022-03-22 17:37:32 +0100

    Initial Commit

---
+++ showcasekit.spm-issue/showcasekit_spm_issueApp.swift
@@ -0,0 +1,17 @@
+//
+//  showcasekit_spm_issueApp.swift
+//  showcasekit.spm-issue
+//
+//  Created by Pakkito on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct showcasekit_spm_issueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
