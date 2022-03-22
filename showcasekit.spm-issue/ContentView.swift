ed5f5637fc427740a39f67fc015d78a7877d22dd
Pakkito <pe@pierlis.com>
2022-03-22 19:39:50 +0100

    Add showcase

--- showcasekit.spm-issue/ContentView.swift
+++ showcasekit.spm-issue/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
@@ -18,4 +19,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+import ShowcaseKit
+fileprivate final class ContentViewShowcase: Showcase {
+    static let path = ShowcasePath.root.underSection(named: "🔗 Flows").inFolder(named: "Auth").underSection(named: "Views")
+    static let title = "AuthenticationView"
+    func makeViewController() -> UIViewController { UIHostingController(rootView: ContentView_Previews.previews) }
 }
